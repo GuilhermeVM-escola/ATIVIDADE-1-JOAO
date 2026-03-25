4a82875aa589070338c6cc5c5127d8698f8f0427
GuilhermeVM-escola <guilherme.vicente.messias@escola.pr.gov.br>
2026-03-25 08:45:55 -0300

    COMENTEI AS QUESTOES PARA REALIZAR

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
+//Soma: Declare duas variáveis, num1 e num2. Realize a soma entre elas e
+//exiba o resultado.
+//Subtração: Crie uma variável para armazenar o resultado da subtração
+//de 50 por 25 e mostre-o no console.
+//Multiplicação: Calcule o produto de dois valores numéricos à sua
+//escolha utilizando o operador *.
+//Divisão: Divida o número 100 por 4 utilizando o operador / e exiba o
+//quociente.
