3b40c80174878d89f7df6f8a39ea2ea09b1378c9
GuilhermeVM-escola <guilherme.vicente.messias@escola.pr.gov.br>
2026-04-01 10:32:41 -0300

    CERTINHO, ACERTADINHO

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ console.log('A PESSOA É:', status);
 
 let nome2 = "João";
 
-console.log('Salve', nome2); 
+console.log(`Salve, ${nome2}`); 
 
 /////////////////////////////////////////////////////
 
